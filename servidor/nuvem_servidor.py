f9e428cc235b4b58c082f9b2643b1d59860da1b1
juliettfigueiredodev <figueiredo.juliett@aluno.ufca.edu.br>
2026-08-17 22:05:37 -0300

    feat: cria  o lado do servidor

--- servidor/nuvem_servidor.py
+++ servidor/nuvem_servidor.py
@@ -1,1 +1,31 @@
-#
+import socket
+import cv2
+import numpy as np
+import json
+from ultralytics import YOLO
+
+model = YOLO('yolo11n.pt')
+vehicle_classes = [2, 3, 5, 7]
+
+sock_servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_servidor.bind(('', 9000))
+
+print("[*] Servidor Cloud de IA aguardando streams de video...")
+
+while True:
+    dados_bytes, endereco_cliente = sock_servidor.recvfrom(65535)
+
+    np_arr = np.frombuffer(dados_bytes, dtype=np.uint8)
+
+    frame_recebido = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    if frame_recebido is not None:
+        results = model.predict(frame_recebido, conf=0.5, classes=vehicle_classes, verbose=False)
+        contagem = len(results[0].boxes)
+        
+        payload = {"carros": contagem}
+        json_str = json.dumps(payload)
+        dados_resposta = json_str.encode()
+
+        dest = (endereco_cliente[0], 9001)
+        sock_servidor.sendto(dados_resposta, dest)
